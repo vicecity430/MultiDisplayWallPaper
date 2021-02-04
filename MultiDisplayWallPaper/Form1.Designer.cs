937fdd93e2c398113bbf27d25a6fd664f568a305
Momo Chiang <vicecity430@Gmail.com>
2021-02-05 05:37:29 +0800

    加入專案檔案。

---
+++ MultiDisplayWallPaper/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace MultiDisplayWallPaper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPath = new System.Windows.Forms.Button();
+            this.textPath1 = new System.Windows.Forms.Label();
+            this.textPath2 = new System.Windows.Forms.Label();
+            this.pictBoxDisplay1 = new System.Windows.Forms.PictureBox();
+            this.pictBoxDisplay2 = new System.Windows.Forms.PictureBox();
+            this.listBoxDisplay1 = new System.Windows.Forms.ListBox();
+            this.listBoxDisplay2 = new System.Windows.Forms.ListBox();
+            this.btnMake = new System.Windows.Forms.Button();
+            this.textSavePath = new System.Windows.Forms.Label();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            this.textDone = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDisplay1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDisplay2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(12, 12);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(80, 30);
+            this.btnPath.TabIndex = 0;
+            this.btnPath.Text = "Path";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // textPath1
+            // 
+            this.textPath1.AutoSize = true;
+            this.textPath1.Location = new System.Drawing.Point(98, 21);
+            this.textPath1.Name = "textPath1";
+            this.textPath1.Size = new System.Drawing.Size(31, 12);
+            this.textPath1.TabIndex = 1;
+            this.textPath1.Text = "Path1";
+            this.textPath1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textPath2
+            // 
+            this.textPath2.AutoSize = true;
+            this.textPath2.Location = new System.Drawing.Point(98, 49);
+            this.textPath2.Name = "textPath2";
+            this.textPath2.Size = new System.Drawing.Size(31, 12);
+            this.textPath2.TabIndex = 1;
+            this.textPath2.Text = "Path2";
+            this.textPath2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictBoxDisplay1
+            // 
+            this.pictBoxDisplay1.Location = new System.Drawing.Point(237, 234);
+            this.pictBoxDisplay1.Name = "pictBoxDisplay1";
+            this.pictBoxDisplay1.Size = new System.Drawing.Size(501, 266);
+            this.pictBoxDisplay1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictBoxDisplay1.TabIndex = 2;
+            this.pictBoxDisplay1.TabStop = false;
+            // 
+            // pictBoxDisplay2
+            // 
+            this.pictBoxDisplay2.Location = new System.Drawing.Point(744, 107);
+            this.pictBoxDisplay2.Name = "pictBoxDisplay2";
+            this.pictBoxDisplay2.Size = new System.Drawing.Size(208, 393);
+            this.pictBoxDisplay2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictBoxDisplay2.TabIndex = 3;
+            this.pictBoxDisplay2.TabStop = false;
+            // 
+            // listBoxDisplay1
+            // 
+            this.listBoxDisplay1.FormattingEnabled = true;
+            this.listBoxDisplay1.ItemHeight = 12;
+            this.listBoxDisplay1.Location = new System.Drawing.Point(26, 124);
+            this.listBoxDisplay1.Name = "listBoxDisplay1";
+            this.listBoxDisplay1.Size = new System.Drawing.Size(87, 376);
+            this.listBoxDisplay1.TabIndex = 4;
+            this.listBoxDisplay1.SelectedIndexChanged += new System.EventHandler(this.listBoxDisplay1_SelectedIndexChanged);
+            // 
+            // listBoxDisplay2
+            // 
+            this.listBoxDisplay2.FormattingEnabled = true;
+            this.listBoxDisplay2.ItemHeight = 12;
+            this.listBoxDisplay2.Location = new System.Drawing.Point(120, 124);
+            this.listBoxDisplay2.Name = "listBoxDisplay2";
+            this.listBoxDisplay2.Size = new System.Drawing.Size(87, 376);
+            this.listBoxDisplay2.TabIndex = 5;
+            this.listBoxDisplay2.SelectedIndexChanged += new System.EventHandler(this.listBoxDisplay2_SelectedIndexChanged);
+            // 
+            // btnMake
+            // 
+            this.btnMake.Location = new System.Drawing.Point(872, 516);
+            this.btnMake.Name = "btnMake";
+            this.btnMake.Size = new System.Drawing.Size(80, 30);
+            this.btnMake.TabIndex = 0;
+            this.btnMake.Text = "Make!";
+            this.btnMake.UseVisualStyleBackColor = true;
+            this.btnMake.Click += new System.EventHandler(this.btnMake_Click);
+            // 
+            // textSavePath
+            // 
+            this.textSavePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textSavePath.Location = new System.Drawing.Point(366, 525);
+            this.textSavePath.Name = "textSavePath";
+            this.textSavePath.Size = new System.Drawing.Size(500, 12);
+            this.textSavePath.TabIndex = 1;
+            this.textSavePath.Text = "SavePath";
+            this.textSavePath.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pBar
+            // 
+            this.pBar.Location = new System.Drawing.Point(822, 553);
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(130, 23);
+            this.pBar.TabIndex = 6;
+            // 
+            // textDone
+            // 
+            this.textDone.Location = new System.Drawing.Point(902, 579);
+            this.textDone.Name = "textDone";
+            this.textDone.Size = new System.Drawing.Size(50, 12);
+            this.textDone.TabIndex = 1;
+            this.textDone.Text = "DONE!";
+            this.textDone.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.textDone.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.pBar);
+            this.Controls.Add(this.listBoxDisplay2);
+            this.Controls.Add(this.listBoxDisplay1);
+            this.Controls.Add(this.pictBoxDisplay2);
+            this.Controls.Add(this.pictBoxDisplay1);
+            this.Controls.Add(this.textPath2);
+            this.Controls.Add(this.textSavePath);
+            this.Controls.Add(this.textDone);
+            this.Controls.Add(this.textPath1);
+            this.Controls.Add(this.btnMake);
+            this.Controls.Add(this.btnPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDisplay1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDisplay2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Label textPath1;
+        private System.Windows.Forms.Label textPath2;
+        private System.Windows.Forms.PictureBox pictBoxDisplay1;
+        private System.Windows.Forms.PictureBox pictBoxDisplay2;
+        private System.Windows.Forms.ListBox listBoxDisplay1;
+        private System.Windows.Forms.ListBox listBoxDisplay2;
+        private System.Windows.Forms.Button btnMake;
+        private System.Windows.Forms.Label textSavePath;
+        private System.Windows.Forms.ProgressBar pBar;
+        private System.Windows.Forms.Label textDone;
+    }
+}
+
---
+++ MultiDisplayWallPaper/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace MultiDisplayWallPaper
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap pictWallPaper;
+        //3640*1920
+        Bitmap pictLeft;
+        //2560*1440
+        Bitmap pictRight;
+        //1080*1920
+        Image image1;
+        Image image2;
+
+        string[] wallpaperFullName1;
+        string[] wallpaperFullName2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            //開視窗1
+            FolderBrowserDialog pathWindow = new FolderBrowserDialog();
+            pathWindow.ShowDialog();
+            textPath1.Text = pathWindow.SelectedPath;
+            //開視窗2
+            pathWindow.ShowDialog();
+            textPath2.Text = pathWindow.SelectedPath;
+            pathWindow.Dispose();
+
+
+            if (textPath1.Text == "" && textPath2.Text == "")
+                return;
+
+            wallpaperFullName1 = null;
+            wallpaperFullName2 = null;
+            wallpaperFullName1 = Directory.GetFiles(textPath1.Text);
+            wallpaperFullName2 = Directory.GetFiles(textPath2.Text);
+
+            if (wallpaperFullName1 == null)
+                Console.WriteLine("NULL");
+            else
+            {
+                Console.WriteLine("wallpaperFullName1 : " + wallpaperFullName1);
+                Console.WriteLine("wallpaperFullName1'lens : " + wallpaperFullName1.Length);
+            }
+                
+
+            //檢查
+            Console.WriteLine("wallpaperFullName1");
+            for (int i = 0; i < wallpaperFullName1.Length; i++)
+            {
+                Console.WriteLine(wallpaperFullName1[i]);
+            }
+            Console.WriteLine("wallpaperFullName2");
+            for (int i = 0; i < wallpaperFullName2.Length; i++)
+            {
+                Console.WriteLine(wallpaperFullName2[i]);
+            }
+            Console.WriteLine("length : " + wallpaperFullName1.Length + " " + wallpaperFullName2.Length);
+            
+
+            string fileNameStr;
+            string fileName;
+            string[] fileNameSplit;
+
+            listBoxDisplay1.Items.Clear();
+            for (int i = 0; i < wallpaperFullName1.Length; i++)
+            {
+                fileNameStr = wallpaperFullName1[i];
+                fileNameSplit = fileNameStr.Split('\\');
+                fileName = fileNameSplit[fileNameSplit.Length - 1];
+                listBoxDisplay1.Items.Add(fileName);
+            }
+            listBoxDisplay2.Items.Clear();
+            for (int i = 0; i < wallpaperFullName2.Length; i++)
+            {
+                fileNameStr = wallpaperFullName2[i];
+                fileNameSplit = fileNameStr.Split('\\');
+                fileName = fileNameSplit[fileNameSplit.Length - 1];
+                listBoxDisplay2.Items.Add(fileName);
+            }
+
+            SetpBar(wallpaperFullName1.Length * wallpaperFullName2.Length);
+            /*
+            int imageCount = 0;
+            ImageCodecInfo myImageCodecInfo = GetEncoderInfo("image/jpeg");
+            System.Drawing.Imaging.Encoder myEncoder = System.Drawing.Imaging.Encoder.Quality;
+            EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 100L);
+            EncoderParameters myEncoderParameters = new EncoderParameters(1);
+            myEncoderParameters.Param[0] = myEncoderParameter;
+
+            for (int i = 0; i < wallpaperFullName1.Length; i++)
+            {
+                for (int j = 0; j < wallpaperFullName2.Length; j++)
+                {
+                    
+                    pictWallPaper = new Bitmap(3640, 1920);
+                    image1 = Image.FromFile(wallpaperFullName1[i]);
+                    image2 = Image.FromFile(wallpaperFullName2[j]);
+                    pictLeft = new Bitmap(image1, 2560, 1440);
+                    pictRight = new Bitmap(image2, 1080, 1920);
+
+                    //pict1
+                    for (int h1 = 0; h1 < 1440; h1++)
+                    {
+                        for (int w1 = 0; w1 < 2560; w1++)
+                        {
+                            pictWallPaper.SetPixel(w1, 480 + h1, pictLeft.GetPixel(w1, h1));
+                        }
+                    }
+
+                    //pict2
+                    for (int h2 = 0; h2 < 1920; h2++)
+                    {
+                        for (int w2 = 0; w2 < 1080; w2++)
+                        {
+                            pictWallPaper.SetPixel(2560 + w2, h2, pictRight.GetPixel(w2, h2));
+                        }
+                    }
+
+                    pictWallPaper.Save(savePath + "\\" + imageCount + ".png", System.Drawing.Imaging.ImageFormat.Png);
+                    pictWallPaper.Save(savePath + "\\" + imageCount + ".jpg", myImageCodecInfo, myEncoderParameters);
+                    imageCount++;
+
+                    pictWallPaper.Dispose();
+                    image1.Dispose();
+                    image2.Dispose();
+                    pictLeft.Dispose();
+                    pictRight.Dispose();
+                }
+            }
+            */
+
+        }
+
+        private void btnMake_Click(object sender, EventArgs e)
+        {
+            //開視窗
+            FolderBrowserDialog pathWindow = new FolderBrowserDialog();
+            pathWindow.ShowDialog();
+            string savePath = pathWindow.SelectedPath;
+            pathWindow.Dispose();
+
+            //確認路徑不為空
+            if (savePath == "")
+                return;
+            Console.WriteLine(savePath);
+            textSavePath.Text = savePath;
+
+            //pBarClear
+            pBar.Value = pBar.Minimum;
+            //DONE! unvisible
+            textDone.Visible = false;
+
+            //檔案序列
+            int imageCount = 0;
+            //JPG設定
+            ImageCodecInfo myImageCodecInfo = GetEncoderInfo("image/jpeg");
+            System.Drawing.Imaging.Encoder myEncoder = System.Drawing.Imaging.Encoder.Quality;
+            EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 100L);
+            EncoderParameters myEncoderParameters = new EncoderParameters(1);
+            myEncoderParameters.Param[0] = myEncoderParameter;
+
+            for (int i = 0; i < wallpaperFullName1.Length; i++)
+            {
+                for (int j = 0; j < wallpaperFullName2.Length; j++)
+                {
+                    pBar.PerformStep();
+                    pictWallPaper = new Bitmap(3640, 1920);
+                    image1 = Image.FromFile(wallpaperFullName1[i]);
+                    image2 = Image.FromFile(wallpaperFullName2[j]);
+                    pictLeft = new Bitmap(image1, 2560, 1440);
+                    pictRight = new Bitmap(image2, 1080, 1920);
+
+                    //pict1
+                    for (int h1 = 0; h1 < 1440; h1++)
+                    {
+                        for (int w1 = 0; w1 < 2560; w1++)
+                        {
+                            pictWallPaper.SetPixel(w1, 480 + h1, pictLeft.GetPixel(w1, h1));
+                        }
+                    }
+
+                    //pict2
+                    for (int h2 = 0; h2 < 1920; h2++)
+                    {
+                        for (int w2 = 0; w2 < 1080; w2++)
+                        {
+                            pictWallPaper.SetPixel(2560 + w2, h2, pictRight.GetPixel(w2, h2));
+                        }
+                    }
+
+                    //pictWallPaper.Save(savePath + "\\" + imageCount + ".png", System.Drawing.Imaging.ImageFormat.Png);
+                    pictWallPaper.Save(savePath + "\\" + imageCount + ".jpg", myImageCodecInfo, myEncoderParameters);
+                    imageCount++;
+
+                    //關閉
+                    pictWallPaper.Dispose();
+                    image1.Dispose();
+                    image2.Dispose();
+                    pictLeft.Dispose();
+                    pictRight.Dispose();
+                }
+            }
+            //show DONE!
+            textDone.Visible = true;
+        }
+
+        private void listBoxDisplay1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictBoxDisplay1.Load(textPath1.Text + "\\" + listBoxDisplay1.SelectedItem.ToString());
+        }
+
+        private void listBoxDisplay2_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+            pictBoxDisplay2.Load(textPath2.Text + "\\" + listBoxDisplay2.SelectedItem.ToString());
+        }
+
+        private ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            int j;
+            ImageCodecInfo[] encoders;
+            encoders = ImageCodecInfo.GetImageEncoders();
+            for (j = 0; j < encoders.Length; ++j)
+            {
+                if (encoders[j].MimeType == mimeType)
+                    return encoders[j];
+            }
+            return null;
+        }
+
+        private void SetpBar(int barMax)
+        {
+            // 設定進度條最小值.
+            pBar.Minimum = 0;
+            // 設定進度條最大值.
+            pBar.Maximum = barMax;
+            // 設定進度條初始值
+            pBar.Value = 0;
+            // 設定每次增加的步長
+            pBar.Step = 1;
+        }
+    }
+}
